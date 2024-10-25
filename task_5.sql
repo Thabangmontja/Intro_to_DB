596f1343548e39234d0259884794f8057d9bd9f7
Thabang Montja <thabangmontja20@gmail.com>
2024-10-25 23:15:22 +0200

    Add task_5.sql to insert a single row into customers table

---
+++ task_5.sql
@@ -0,0 +1,10 @@
+-- File: task_5.sql
+-- Script to insert a single row into the customers table
+
+INSERT INTO customers (customer_id, customer_name, email, address)
+VALUES (1, 'Cole Baidoo', 'cbaidoo@sandtech.com', '123 Happiness Ave.');
+
+SHOW CREATE TABLE customers;
+
+
+
